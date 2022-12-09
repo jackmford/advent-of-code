d42894d231047d45ea8a27936a8f32a84fa77df5
jackmford <jackmitchellfordyce@gmail.com>
2022-12-09 11:17:41 -0600

    main function

--- day-one/day-1.py
+++ day-one/day-1.py
@@ -1,15 +1,16 @@
-f = open('input.txt', 'r')
-
-calorie_ctr = 0
-
-calorie_totals = []
-
-for line in f:
-    if line != '\n':
-        calorie_ctr += int(line.strip())
-    else:
-        calorie_totals.append(calorie_ctr)
-        calorie_ctr = 0
-
-sorted_cals = sum(sorted(calorie_totals)[-3:])
-print(sorted_cals)
+if __name__ == '__main__':
+    f = open('input.txt', 'r')
+    
+    calorie_ctr = 0
+    
+    calorie_totals = []
+    
+    for line in f:
+        if line != '\n':
+            calorie_ctr += int(line.strip())
+        else:
+            calorie_totals.append(calorie_ctr)
+            calorie_ctr = 0
+    
+    sorted_cals = sum(sorted(calorie_totals)[-3:])
+    print(sorted_cals)
