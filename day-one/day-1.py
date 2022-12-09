e07cdc97909dd795ad31de4fa41193907f33ab38
jackmford <jackmitchellfordyce@gmail.com>
2022-12-09 11:14:43 -0600

    i didn't realize there was 2 parts lol

--- day-one/day-1.py
+++ day-one/day-1.py
@@ -1,20 +1,15 @@
 f = open('input.txt', 'r')
 
-position = 0
-calories = 0
-
-position_ctr = 1
 calorie_ctr = 0
+
+calorie_totals = []
 
 for line in f:
     if line != '\n':
         calorie_ctr += int(line.strip())
     else:
-        if calorie_ctr > calories:
-            calories = calorie_ctr
-            position = position_ctr
-        position_ctr += 1
+        calorie_totals.append(calorie_ctr)
         calorie_ctr = 0
 
-print(position)
-print(calories)
+sorted_cals = sum(sorted(calorie_totals)[-3:])
+print(sorted_cals)
