8d9e3633ea2e5ef36463d0863955f9a2cd607c41
jackmford <jackmitchellfordyce@gmail.com>
2023-12-15 10:01:25 -0600

    p2:

--- 2023/day_three/redo.py
+++ 2023/day_three/redo.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 with open('puzzle_input.txt', 'r') as file:
     lines = file.readlines()
 
@@ -5,13 +6,18 @@ graph = [[char for char in line.strip('\n')] for line in lines]
 row_length = len(graph[0])
 num = 0
 total = 0
+total2 = 0
 valid = False
+gear = False
+gears = defaultdict(list)
 for x in range(len(graph)):
     if valid:
-        print(num)
         total+=num
+    if gear:
+        gears[(gearx, geary)].append(num)
     num = 0
     valid = False
+    gear = False
     for y in range(row_length):
         if graph[x][y].isdigit():
             num = num*10+int(graph[x][y])
@@ -20,11 +26,21 @@ for x in range(len(graph)):
                     if y+z > 0 and x+j > 0 and x+j < row_length and y+z < row_length:
                         if not(graph[x+j][y+z].isdigit()) and graph[x+j][y+z] != '.':
                             valid=True
+                            if graph[x+j][y+z] == '*':
+                                gear = True
+                                gearx = x+j
+                                geary = y+z
         else:
             if valid:
-                print(num)
                 total+=num
+            if gear:
+                gears[(gearx, geary)].append(num)
             num = 0
             valid = False
+            gear = False
+for k, v in gears.items():
+    if len(v) == 2:
+        total2 += gears[k][0]*gears[k][1]
 print(total)
+print(total2)
 
